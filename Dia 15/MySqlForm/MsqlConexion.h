9a8cd2b905d6b272516797f299d89fe4b3cf9e7c
Mariano Crespo López <myb1999@hotmail.com>
2023-11-30 20:44:28 +0100

    Subir ejercicio semiterminado

--- Dia 15/MySqlForm/MsqlConexion.h
+++ Dia 15/MySqlForm/MsqlConexion.h
@@ -3,6 +3,7 @@
 #include <mysql.h>
 #include <Windows.h>
 #include <string>
+#include <vector>
 
 class MsqlConexion
 {
@@ -10,6 +11,7 @@ private:
 
 	MYSQL* conectar;  // La variable tipo MYSQL para establecer el resultado de la conexión
 	MYSQL_ROW fila; // Variable MYSQL en la que albergaremos datos de una fila de la respuesta de la consulta.
+	MYSQL_RES* resultado; // La variable tipo MYSQL para establecer el resultado de la respuesta de una consulta
 
 	int puerto;
 	std::string servidor;
@@ -36,20 +38,64 @@ public:
 	}
 	~MsqlConexion() = default;
 
-	MYSQL_RES* resultado; // La variable tipo MYSQL para establecer el resultado de la respuesta de una consulta
 
-	void LeerTodo(std::string query)
+	std::vector<std::string> LeerTodo()
 	{
+		std::vector<std::string> resultados;
+		std::string query = "SELECT * FROM usuario";
 		Conexion();
 		if (mysql_query(conectar, query.c_str()))
 		{
 			System::Windows::Forms::MessageBox::Show("No se pudo conectar");
 		}
 		resultado = mysql_store_result(conectar);
+		if (resultado)
+		{
+			int numColumnas = mysql_num_fields(resultado);
+			MYSQL_ROW fila;
+
+			while ((fila = mysql_fetch_row(resultado)))
+			{
+				for (int i = 0; i < numColumnas; ++i)
+				{
+					resultados.push_back(fila[i] ? fila[i] : "NULL");
+				}
+			}
+			mysql_free_result(resultado);
+		}
+		else
+		{
+			System::Windows::Forms::MessageBox::Show("Error al obtener el resultado de la consulta");
+		}
+		Desconexion();
+		return resultados;
+	}
+	void Insertar(std::string &nombre, std::string &pass)
+	{
+		std::string query = "INSERT INTO usuario (Nombre, Pass) VALUES ('" + nombre + "','" + pass + "');";
+
+		Conexion();
+		if (mysql_query(conectar, query.c_str()));
+		{
+			resultado = mysql_store_result(conectar);
+		}
 		Desconexion();
 	}
-	void Insertar(std::string query)
+	void Actualizar(std::string &nombre, std::string &pass)
 	{
+		std::string query = "UPDATE usuario SET Nombre ='" + nombre + "', Pass = '" + pass + "');";
+
+		Conexion();
+		if (mysql_query(conectar, query.c_str()));
+		{
+			resultado = mysql_store_result(conectar);
+		}
+		Desconexion();
+	}
+	void Borrar(int &id)
+	{
+		std::string query = "DELETE FROM usuario WHERE ID = " + std::to_string(id);
+
 		Conexion();
 		if (mysql_query(conectar, query.c_str()));
 		{
